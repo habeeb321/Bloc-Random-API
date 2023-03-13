c87f1ee61c13b08c2ac0672c41180e20565433d5
habeebu <habeebrh321@gmail.com>
2023-03-13 10:06:14 +0530

    project completed

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,24 +1,36 @@
+import 'dart:developer';
+
+import 'package:bloc_random_api/logic/home_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
+    log('entire page rebuild');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Api Fetching'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Api Fetching'),
-          ],
-        ),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          log('Specific section is rebuilding');
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(state.result.toString()),
+              ],
+            ),
+          );
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () {
+          context.read<HomeBloc>().add(FetchApiEvent());
+        },
         label: const Text('Add'),
       ),
     );
